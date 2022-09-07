a610dc70b9a89e23b70e4ca820e97e6796ad625b
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-09-07 16:55:30 +1200

    Mobile changes to policy componet

--- src/Components/Forms/SignUpContent/SignUpFormPolicy.js
+++ src/Components/Forms/SignUpContent/SignUpFormPolicy.js
@@ -1,16 +1,20 @@
 import classes from "./SignUpFormPolicy.module.css";
+import useWindowSize from "../../../Hooks/use-windowSize";
 import "../../../index.css";
 
 const SignUpFormPolicy = () => {
+  const { isMobileView } = useWindowSize();
   return (
     <div className={classes.container}>
       <div className="row">
         <div className="col-9">
-          <p className={classes.policyText}>
-            By signing up you agree to our privacy policy. You can unsubscribe
-            or change your preferences at any time by clicking the link in any
-            emails.
-          </p>
+          {!isMobileView && (
+            <p className={classes.policyText}>
+              By signing up you agree to our privacy policy. You can unsubscribe
+              or change your preferences at any time by clicking the link in any
+              emails.
+            </p>
+          )}
         </div>
         <div className="col-3"></div>
       </div>
