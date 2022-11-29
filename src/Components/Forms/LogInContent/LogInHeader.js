515120321b9f27e020d522478761e026e4bd781b
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-11-29 17:15:38 +1300

    Update LogInHeader.js

--- src/Components/Forms/LogInContent/LogInHeader.js
+++ src/Components/Forms/LogInContent/LogInHeader.js
@@ -3,7 +3,7 @@ import classes from "./LogInHeader.module.css";
 const LogInHeader = () => {
   return (
     <div className={classes.container}>
-      <h1 className={classes.header}>Login to URBAN</h1>
+      <h1 className={classes.header}>Welcome Back</h1>
       <p className={classes.smallText}>
         Login to explore our current exhibits from around the world.
       </p>
